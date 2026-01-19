d4ba718387e79d05abda25cd0f201d7dd37bc7ac
Anton Dubovik <anton_dubovik@epam.com>
2026-01-19 15:33:55 +0000

    fix: add citations to consumer

---
+++ aidial_adapter_anthropic/adapter/_claude/citations.py
@@ -0,0 +1,60 @@
+import logging
+from typing import assert_never
+
+from anthropic.types.beta import (
+    BetaCitationCharLocation as CitationCharLocation,
+)
+from anthropic.types.beta import (
+    BetaCitationContentBlockLocation as CitationContentBlockLocation,
+)
+from anthropic.types.beta import (
+    BetaCitationPageLocation as CitationPageLocation,
+)
+from anthropic.types.beta import (
+    BetaCitationSearchResultLocation as CitationSearchResultLocation,
+)
+from anthropic.types.beta import (
+    BetaCitationsWebSearchResultLocation as CitationsWebSearchResultLocation,
+)
+from anthropic.types.beta import BetaTextCitation as TextCitation
+
+from aidial_adapter_anthropic.dial.consumer import Consumer
+
+
+async def _add_document_citation(
+    consumer: Consumer, prompt: ClaudePrompt, document_index: int
+):
+    resource = prompt.get_dial_resource(document_index)
+    attachment = None if resource is None else resource.to_attachment()
+
+    # NOTE: multiple citations to the same document are merged into one citation
+    # until we find a better API to handle citations embedded in text.
+    display_index = await consumer.add_citation_attachment(
+        document_id=document_index, document=attachment
+    )
+
+    # NOTE: avoid adding citation URLs into the generated content,
+    # since such references aren't easily portable (e.g. when a conversion is duplicated).
+    consumer.append_content(f"[{display_index}]")
+
+
+async def create_citations(
+    consumer: Consumer, prompt: ClaudePrompt, citation: TextCitation
+):
+    match citation:
+        case CitationCharLocation(document_index=document_index):
+            await _add_document_citation(consumer, prompt, document_index)
+
+        case CitationPageLocation(document_index=document_index):
+            await _add_document_citation(consumer, prompt, document_index)
+
+        # custom document aren't supported yet
+        case CitationContentBlockLocation():
+            pass
+        # web search isn't supported yet
+        case CitationsWebSearchResultLocation():
+            pass
+        case CitationSearchResultLocation():
+            pass
+        case _:
+            assert_never(citation)
