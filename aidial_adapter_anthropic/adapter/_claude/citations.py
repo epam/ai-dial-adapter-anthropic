de24980aef43085db5f4aaf21119ef7e168c114b
Anton Dubovik <anton_dubovik@epam.com>
2026-04-08 12:41:32 +0100

    feat!: Consumer interface made async

--- aidial_adapter_anthropic/adapter/_claude/citations.py
+++ aidial_adapter_anthropic/adapter/_claude/citations.py
@@ -22,7 +22,7 @@ from aidial_adapter_anthropic.dial.consumer import Consumer
 from aidial_adapter_anthropic.dial.resource import DialResource
 
 
-def _add_document_citation(
+async def _add_document_citation(
     consumer: Consumer,
     get_document: Callable[[int], DialResource | None],
     document_index: int,
@@ -38,10 +38,10 @@ def _add_document_citation(
 
     # NOTE: avoid adding citation URLs into the generated content,
     # since such references aren't easily portable (e.g. when a conversion is duplicated).
-    consumer.append_content(f"[{display_index}]")
+    await consumer.append_content(f"[{display_index}]")
 
 
-def create_citations(
+async def create_citations(
     consumer: Consumer,
     get_document: Callable[[int], DialResource | None],
     citation: TextCitation,
@@ -51,7 +51,7 @@ def create_citations(
             CitationCharLocation(document_index=document_index)
             | CitationPageLocation(document_index=document_index)
         ):
-            _add_document_citation(consumer, get_document, document_index)
+            await _add_document_citation(consumer, get_document, document_index)
 
         # Custom document aren't supported yet
         case CitationContentBlockLocation():
