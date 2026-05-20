40a7c7ba4495937f94f318cb5b4ee62b1dbc8565
Alex Chernyshov <alexfoxalt@gmail.com>
2026-05-20 16:45:47 +0300

    fix: cleanup comment

--- aidial_adapter_anthropic/adapter/_claude/config.py
+++ aidial_adapter_anthropic/adapter/_claude/config.py
@@ -18,8 +18,6 @@ class ClaudeConfiguration(ExtraForbidModel):
 
 
 class ClaudeConfigurationWithThinking(ClaudeConfiguration):
-    # NOTE: once migrated to Pydantic v2 we can use TypeAdapter over
-    # the anthropic's ThinkingConfigParam class directly.
     thinking: ThinkingConfigParam | dict[str, Any] | None = None
 
 
